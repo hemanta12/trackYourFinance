58e62760b980d54db313c76beb677cece346587c
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2025-01-10 12:18:37 -0800

    Initial project setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000 ;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Basic route for testing
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
