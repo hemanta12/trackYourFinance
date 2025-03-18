f2e923dbfe58177beede1272112d608410dd11ea
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2025-03-17 21:13:31 -0700

    add: render backend url for api.js and authSlice.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:8080/api";
-const API_BASE_URL = "http://localhost:49684/api";
+// const API_BASE_URL = "http://localhost:49684/api";
+const API_BASE_URL = "https://trackyourfinance-backend.onrender.com";
 
 // const token = localStorage.getItem("token");
 
